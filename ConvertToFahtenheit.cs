5ea4de56bd2519318d60713c3633eee062800448
ITHanan <155847905+ITHanan@users.noreply.github.com>
2024-09-25 12:14:36 +0200

    Add project files.

---
+++ ConvertToFahtenheit.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace TemperaturCelsiusTillFahrenheit
+{
+    public class ConvertToFahtenheit
+    {
+        public int ConvertCelsiusTillFahtenheit(int tempratureInCelsius) 
+        { 
+            int tempraturInFahrenheit = (tempratureInCelsius * 9 / 5) + 32;
+
+            return tempraturInFahrenheit;
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TemperaturCelsiusTillFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //från Celsius till Fahrenheit
+
+            Console.WriteLine("Enter the temprature in Celsius: ");
+
+            string trmpratureInCelsiusSomString = Console.ReadLine()!;
+
+            int tempratureInCelsius = Convert.ToInt32(trmpratureInCelsiusSomString);
+
+            ConvertToFahtenheit myconverter = new ConvertToFahtenheit();
+
+            int tempraturInFahrenheit = myconverter.ConvertCelsiusTillFahtenheit(tempratureInCelsius);
+
+            Console.WriteLine($"The temperatuer in Fahrenheit is : {tempraturInFahrenheit}");
+
+
+
+        }
+    }
+}
